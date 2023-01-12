6ae27d4da120571f7ffbc5da1321474ef2dfe877
Armada Afga Adrian Nasar <armada.nasar@pinhome.id>
2023-01-12 19:31:07 +0700

    feat: initial commit

---
+++ lib/core/api/post_api.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:retrofit_dio_example/core/models/post.dart';
+
+part 'post_api.g.dart';
+
+@RestApi()
+abstract class PostApi {
+  factory PostApi(Dio dio, {String baseUrl}) = _PostApi;
+
+  @GET('/posts')
+  Future<List<Post>> getPosts();
+}
